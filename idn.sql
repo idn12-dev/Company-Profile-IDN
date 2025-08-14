75858a48d3f7a670c5e4e3da91f400a42aa99816
idn12-dev <inovasidigitalnusantara12@gmail.com>
2025-08-14 14:20:18 +0700

    Add database SQL file

---
+++ idn.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 14, 2025 at 09:17 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `idn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache`
+--
+-- Error reading structure for table idn.cache: #1932 - Table &#039;idn.cache&#039; doesn&#039;t exist in engine
+-- Error reading data for table idn.cache: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `idn`.`cache`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache_locks`
+--
+-- Error reading structure for table idn.cache_locks: #1932 - Table &#039;idn.cache_locks&#039; doesn&#039;t exist in engine
+-- Error reading data for table idn.cache_locks: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `idn`.`cache_locks`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+-- Error reading structure for table idn.failed_jobs: #1932 - Table &#039;idn.failed_jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table idn.failed_jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `idn`.`failed_jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+-- Error reading structure for table idn.jobs: #1932 - Table &#039;idn.jobs&#039; doesn&#039;t exist in engine
+-- Error reading data for table idn.jobs: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `idn`.`jobs`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_batches`
+--
+-- Error reading structure for table idn.job_batches: #1932 - Table &#039;idn.job_batches&#039; doesn&#039;t exist in engine
+-- Error reading data for table idn.job_batches: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `idn`.`job_batches`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+-- Error reading structure for table idn.migrations: #1932 - Table &#039;idn.migrations&#039; doesn&#039;t exist in engine
+-- Error reading data for table idn.migrations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `idn`.`migrations`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+-- Error reading structure for table idn.password_reset_tokens: #1932 - Table &#039;idn.password_reset_tokens&#039; doesn&#039;t exist in engine
+-- Error reading data for table idn.password_reset_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `idn`.`password_reset_tokens`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+-- Error reading structure for table idn.sessions: #1932 - Table &#039;idn.sessions&#039; doesn&#039;t exist in engine
+-- Error reading data for table idn.sessions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `idn`.`sessions`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table idn.users: #1932 - Table &#039;idn.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table idn.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `idn`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
